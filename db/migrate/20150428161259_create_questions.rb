b13ecf233d273f8563e4244d7e07caaf93a99577
BootCoder <bootcoder@gmail.com>
2015-04-28 09:16:27 -0700

    add feilds to question and answer migrations

--- db/migrate/20150428161259_create_questions.rb
+++ db/migrate/20150428161259_create_questions.rb
@@ -2,7 +2,10 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
 
+      t.string :body
+
       t.timestamps
+
     end
   end
 end
--- db/migrate/20150428161305_create_answers.rb
+++ db/migrate/20150428161305_create_answers.rb
@@ -2,6 +2,11 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
 
+      t.string :body
+      t.boolean :correct
+
+      t.belongs_to :question
+
       t.timestamps
     end
   end

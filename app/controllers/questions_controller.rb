27fb20a8329997324efdd869c1507737123adcd1
BootCoder <bootcoder@gmail.com>
2015-04-28 09:33:45 -0700

    add link to question on q-index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,19 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render new_question_path
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
 end

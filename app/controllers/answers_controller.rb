365ac514debed459c35d1a685e1597bb054f4b59
BootCoder <bootcoder@gmail.com>
2015-04-28 10:54:42 -0700

    a-cont add index new create & answer_params methods

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,30 @@
 class AnswersController < ApplicationController
 
+  def index
+    @question = Question.where(id: params[:id]).first
+    @answers = @question.answers
+  end
+
   def new
     @question = Question.where(id: params[:question_id]).first
     @answer = Answer.new
   end
+
+  def create
+    @question = Question.where(id: params[:question_id]).first
+    @answer = @question.answers.create(answer_params)
+    if @answer.save
+      redirect_to root_path
+    else
+      render new_question_answer_path
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body, :correct)
+  end
+
 
 end
